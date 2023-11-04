0163a8b80ee11cbd55a7482ca3a9bda5f404485c
Lucas Fernandes <lucasfernandesprogrammer@gmail.com>
2023-11-04 13:45:02 -0300

    :lipstick: feat: página de login UI

--- src/app/api/user.ts
+++ src/app/api/user.ts
@@ -7,8 +7,8 @@ export default withIronSessionApiRoute(
     res.send({ user: req.session.user });
   },
   {
-    cookieName: "myapp_cookiename",
-    password: "complex_password_at_least_32_characters_long",
+    cookieName: process.env.SESSION_COOKIE_NAME as string,
+    password: process.env.SESSION_COOKIE_PASSWORD as string,
     // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
     cookieOptions: {
       secure: process.env.NODE_ENV === "production",
